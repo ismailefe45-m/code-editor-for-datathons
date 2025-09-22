fa41ee3d633bc3f2d47651491d49a9ad3d5bed86
İsmail efe KARAASLAN <karaaslanlaryazilim@gmail.com>
2025-09-22 09:50:20 +0300

    first commit

---
+++ ediiter.py
@@ -0,0 +1,12 @@
+from rapidfuzz import fuzz
+"""hangi fonksiyonlar olacak
+
+similarty check benzer kelimeleri yakalayıp editleyecek
+otofiller boşluklu yazılmış kelimeleri  birleştiricek
+longformer take slangs from a dictiolnary and will change them with longer version
+
+"""
+thechoosensimilars = ["selam"]
+def similarity_check(will_edited):
+def otofiller(will_edited):
+print(b)
