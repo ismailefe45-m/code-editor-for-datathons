0b55f1c81cd0bcb8c2a4618d35fcd88c3f74b8d8
İsmail efe KARAASLAN <karaaslanlaryazilim@gmail.com>
2025-09-24 08:31:06 +0300

    longformer and smilaritycheck add

--- ediiter.py
+++ ediiter.py
@@ -1,4 +1,5 @@
 from rapidfuzz import fuzz
+import re
 """hangi fonksiyonlar olacak
 
 similarty check benzer kelimeleri yakalayıp editleyecek
@@ -6,7 +7,22 @@ otofiller boşluklu yazılmış kelimeleri  birleştiricek
 longformer take slangs from a dictiolnary and will change them with longer version
 
 """
-
+choosens = [""]
+slang_dictionaray={
+    "no.":"numara"
+}
 def similarity_check(will_edited):
+    for choosen in choosens:
+        if fuzz.ratio(choosen,will_edited)>70:
+            return choosen
+        else: return will_edited
+
 def otofiller(will_edited):
+    for i in will_edited:
+        pass
 def longformer(will_edited): 
+    for slang in slang_dictionaray:
+        if will_edited==slang:
+            return slang_dictionaray[slang]
+
+print(longformer("no."))
